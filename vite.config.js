6549c0375e5ad14ecfc9a48a01f096c8504735f9
belelegh <144286852+belelegh@users.noreply.github.com>
2026-02-09 14:43:16 -0500

    ready

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
+
+  base: '/amazon-clone-2026/',
+
+ gi plugins: [react()],
 })
